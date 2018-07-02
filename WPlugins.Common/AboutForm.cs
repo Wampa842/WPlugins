afd8ec9680d80cfc3dc821518aababd16e00f3fc
wampa842 <wampa842@gmail.com>
2018-07-02 03:03:04 +0200

    fixed copyright notice

--- WPlugins.Common/AboutForm.cs
+++ WPlugins.Common/AboutForm.cs
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with WPlugins.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
